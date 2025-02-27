fca45902008b3fef5539c58590c7c93cdd2742c3
Richard <ho.richard05@gmail.com>
2025-02-26 16:22:33 -0800

    v2

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -3,3 +3,14 @@ import pytest
 from foo_bar_baz import foo_bar_baz
 
 #Add testcases Here
+
+def test_normal():
+    assert foo_bar_baz(1) == "1"
+
+    assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
+
+def test_negative():
+    assert foo_bar_baz(-1) == ""
+
+def test_large():
+    assert foo_bar_baz(20) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz 16 17 Foo 19 Bar"
