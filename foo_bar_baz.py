56bb8c14cbf3239a3cd001dc35eaabeda08f1b83
Richard <ho.richard05@gmail.com>
2025-02-26 16:11:37 -0800

    v1

--- foo_bar_baz.py
+++ foo_bar_baz.py
@@ -1,3 +1,5 @@
+import pytest
+
 def foo_bar_baz(n: int) -> str:
     return_str = ""
     for i in range(1, n + 1, 1):
@@ -12,3 +14,12 @@ def foo_bar_baz(n: int) -> str:
         if i < n:
             return_str += " "
     return return_str
+
+def test_foo_bar_baz():
+    assert foo_bar_baz(1) == "1"
+
+    assert foo_bar_baz(5) == "1 2 Foo 4 Bar"
+
+    assert foo_bar_baz(-1) == ""
+
+    assert foo_bar_baz(20) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz 16 17 Foo 19 Bar"
